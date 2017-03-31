7baa30e10cc6a4c97f4db6a9867fcc44d7414390
azenzin <a.zenzin.dev@gmail.com>
2017-03-31 18:39:17 +0300

    Mailchimp

--- wp-content/themes/betheme-child/footer.php
+++ wp-content/themes/betheme-child/footer.php
@@ -177,5 +177,7 @@ if( $back_to_top_class == 'hide' ){
 <!-- wp_footer() -->
 <?php wp_footer(); ?>
 
+<script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us8.list-manage.com","uuid":"9f71368dc454f7ad28ffeb362","lid":"3b7ee7f5cd"}) })</script>
+
 </body>
 </html>
