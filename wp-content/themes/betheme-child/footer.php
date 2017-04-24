7f6a2368630fe3e575731362a3b6344e19e691b9
azenzin <a.zenzin.dev@gmail.com>
2017-04-24 10:34:34 +0300

    remove footer GA

--- wp-content/themes/betheme-child/footer.php
+++ wp-content/themes/betheme-child/footer.php
@@ -178,15 +178,6 @@ if( $back_to_top_class == 'hide' ){
 <?php wp_footer(); ?>
 
 <script type="text/javascript" src="//s3.amazonaws.com/downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script><script type="text/javascript">require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us8.list-manage.com","uuid":"9f71368dc454f7ad28ffeb362","lid":"3b7ee7f5cd"}) })</script>
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-97058814-1', 'auto');
-    ga('send', 'pageview');
-
-</script>
 </body>
 </html>
